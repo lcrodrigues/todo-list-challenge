c2e467974a827ae5d6c1bb9577e4619d5ddbc4c4
Leonardo Rodrigues <leonardo.rodrigues@poatek.com>
2023-10-05 14:07:10 -0300

    Add svg assets

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -1,1 +1,6 @@
 export { default as AppLogo } from "./app_logo.svg";
+export { default as PlusIcon } from "./ic_plus.svg";
+export { default as CheckIcon } from "./ic_check.svg";
+export { default as DefaultDeleteIcon } from "./ic_default_delete.svg";
+export { default as HoverDeleteIcon } from "./ic_hover_delete.svg";
+export { default as ClipboardIcon } from "./ic_clipboard.svg";
